744662664dbff4776ef68b2a9ebcbfe898b2cc6c
Gabriel Rocha <rocha_gabriel@id.uff.br>
2025-07-18 11:52:01 -0300

    Estilizando a HomePage

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,39 +1,55 @@
 import { NavLink, Outlet } from "react-router-dom";
 
 const HomePage = () => {
-
   return (
-    <div className="row">
-      <div className="col-lg-2">
-        <nav className="nav nav-pills d-flex flex-column">
-          <h5>Categorias</h5>
-          <NavLink className="nav-link" aria-current="page" to="/">
-            Todos
-          </NavLink>
-          <NavLink className="nav-link" to="/Cimento">
-            Cimento
-          </NavLink>
-          <NavLink className="nav-link" to="/Madeira">
-            Madeira
-          </NavLink>
-          <NavLink className="nav-link" to="/Ferramentas">
-            Ferramentas
-          </NavLink>
-          <NavLink className="nav-link" to="/Tintas">
-            Tintas
-          </NavLink>
-          <NavLink className="nav-link" to="/Telhas">
-            Telhas
-          </NavLink>
-          <NavLink className="nav-link" to="/Pisos">
-            Pisos
-          </NavLink>
-        </nav>
-      </div>
-      <div className="col-lg-10">
-        <Outlet />
+    <div className="container-fluid py-4">
+      <div className="row">
+        {/* Sidebar */}
+        <div className="col-lg-2 mb-3">
+          <div className="card shadow-sm">
+            <div className="card-body">
+              <h5 className="card-title mb-3">Categorias</h5>
+              <nav className="nav nav-pills flex-column gap-2">
+                <NavLink className="nav-link" to="/">
+                  <i className="bi bi-box-seam me-2"></i>
+                  Todos
+                </NavLink>
+                <NavLink className="nav-link" to="/Cimento">
+                  <i className="bi bi-bricks me-2"></i>
+                  Cimento
+                </NavLink>
+                <NavLink className="nav-link" to="/Madeira">
+                  <i className="bi bi-tree me-2"></i>
+                  Madeira
+                </NavLink>
+                <NavLink className="nav-link" to="/Ferramentas">
+                  <i className="bi bi-wrench-adjustable-circle me-2"></i>
+                  Ferramentas
+                </NavLink>
+                <NavLink className="nav-link" to="/Tintas">
+                  <i className="bi bi-palette2 me-2"></i>
+                  Tintas
+                </NavLink>
+                <NavLink className="nav-link" to="/Telhas">
+                  <i className="bi bi-house-door-fill me-2"></i>
+                  Telhas
+                </NavLink>
+                <NavLink className="nav-link" to="/Pisos">
+                  <i className="bi bi-grid-3x3-gap-fill me-2"></i>
+                  Pisos
+                </NavLink>
+              </nav>
+            </div>
+          </div>
+        </div>
+
+        {/* Conteúdo */}
+        <div className="col-lg-10">
+          <Outlet />
+        </div>
       </div>
     </div>
   );
 };
+
 export default HomePage;
